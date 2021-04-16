5993fbb26dd35fd6ce30c517edb835f6d79acead
LonelyDragoness <60567635+LonelyDragoness@users.noreply.github.com>
2021-04-16 11:43:18 +0300

    refactor: remove redundant code in Jewel.java

--- java/src/main/java/CollectionsTask/Jewel.java
+++ java/src/main/java/CollectionsTask/Jewel.java
@@ -1,15 +1,11 @@
 package CollectionsTask;
 
-import CollectionsTask.classification.GemPreciousness;
 import CollectionsTask.classification.GemTransparency;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Jewel {
-
     public static int countPrice(List<Gemstone> jewel) {
         int summaryGemPrice = 0;
         for (Gemstone gemstone : jewel) {
@@ -37,21 +33,4 @@ public class Jewel {
         }
         return gemsWithSpecificTransparency;
     }
-
-    public static List<Gemstone> sortGemsByPreciousness(List<Gemstone> jewel) {
-        List<Gemstone> gemsSortedByPreciousness = new ArrayList<>();
-        int preciousnessOrdinal = 0;
-        for (Gemstone gemstone : jewel) {
-            if (gemstone.getPreciousness().ordinal() > preciousnessOrdinal) {
-
-            }
-        }
-        return gemsSortedByPreciousness;
-    }
-
-
-    public static int compare(Gemstone o1, Gemstone o2) {
-        return o1.getPreciousness().ordinal() - o2.getPreciousness().ordinal();
-    }
-
 }
